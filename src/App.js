e97a0a3ba7f0c21fd2aa82681b2e840b4b2807d8
DimitrisZar <dimitriszarahanis@gmail.com>
2020-07-09 19:09:20 +0300

    render sidebar and mainview on main app comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import MainView from './components/mainView'
+import MainView from './components/mainView';
+import Sidebar from './components/sidebar';
 import './App.css';
-
-
 
 function App() {
   return (
-    <div className="App">
-      <MainView />
+    <div className="App container-fluid p-0" style={{ display: 'flex' }}>
+      <Sidebar />
+      <MainView name={[1, 2, 3]} />
     </div>
   );
 }
