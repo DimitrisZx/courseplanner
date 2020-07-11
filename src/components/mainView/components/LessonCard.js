62186afdaab01e8d79e8ce61928657edb673585a
DimitrisZar <dimitriszarahanis@gmail.com>
2020-07-11 21:19:24 +0300

    add workshop choices menus

--- src/components/mainView/components/LessonCard.js
+++ src/components/mainView/components/LessonCard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { addLesson, removeLesson, selectSelectedLessons, selectUser } from 'features/counter/counterSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { not } from 'utils'
 const LessonCard = ({ lesson }) => {
-  const { name, semester } = lesson;
+  const [isDropdownExpanded, setIsDropdownExpanded] = useState(false);
+  const { name, semester, type } = lesson;
   const dispatch = useDispatch();
   const selectedLessons = useSelector(selectSelectedLessons);
   const { currentSemester } = useSelector(selectUser);
@@ -14,19 +15,42 @@ const LessonCard = ({ lesson }) => {
     not(isLessonSelected) || selectedLessons.length === 0
       ? dispatch(addLesson({ ...lesson }))
       : dispatch(removeLesson({ ...lesson }));
+    if (lesson.type === 'workshop') {
+      setIsDropdownExpanded(!isDropdownExpanded)
+    }
   };
+  const workShopClick = () => {
 
-  const buttonClasses = (isSelected, semester) => {
-    return `btn btn${not(isSelected) ? '-outline' : ''}-${semester === currentSemester ? 'success' : 'secondary'} mr-2`
   }
 
+  const buttonClasses = (isSelected, semester, type) => {
+    return `btn btn${not(isSelected) ? '-outline' : ''}-${semester === currentSemester ? 'success' : 'secondary'} mr-2 ${type === 'workshop' && 'dropdown-toggle'}`
+  }
+  console.log(lesson)
   return (
-    <div
-      className={buttonClasses(isSelected, semester)}
-      onClick={() => handleClick(name)}
-    >
-      {name}
-    </div>
+    <span className='btn-container' style={{ position: 'relative' }}>
+      <button
+        className={buttonClasses(isSelected, semester, type)}
+        onClick={() => handleClick(name)}
+        id={`lesson-${name}-${type}`}
+        type={'button'}
+      >
+        {name}
+      </button>
+      {type === 'workshop' && isDropdownExpanded &&
+        <div
+          style={{ display: 'block', top: '35px' }}
+          className="dropdown-menu"
+        >
+          {lesson.days.map(day =>
+            <a
+              className="dropdown-item" href="#"
+            >
+              {`${day.day} ${day.hours[0][0]}:00-${day.hours[0][1]}:00`}
+            </a>)}
+        </div>
+      }
+    </span>
   )
 }
 
