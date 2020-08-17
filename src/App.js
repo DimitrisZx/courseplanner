0d2d5c7c12be1ff1a5e61b91464e0b72e6d3202d
DimitrisZar <dimitriszarahanis@gmail.com>
2020-08-17 17:54:12 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 import MainView from './components/mainView';
 import Sidebar from './components/sidebar';
 import './App.css';
+import LoginForm from './components/loginForm'
 import {
-  getLessonsAsync
+  getLessonsAsync,
 } from 'features/counter/counterSlice';
 import { useDispatch } from 'react-redux';
 
@@ -13,8 +18,15 @@ function App() {
   useEffect(() => { dispatch(getLessonsAsync()) });
   return (
     <div className="App container-fluid p-0" style={{ display: 'flex' }}>
-      <Sidebar />
-      <MainView name={[1, 2, 3]} />
+      <Router>
+        <Route exact path='/auth'>
+          <LoginForm />
+        </Route>
+        <Route exact path='/my-schedule'>
+          <Sidebar />
+          <MainView name={[1, 2, 3]} />
+        </Route>
+      </Router>
     </div>
   );
 }
